c18efae86c75b80b27a22ccd49f8f301ca1cc4ba
SOHAM-THUMMAR <sohamthummar04@gmail.com>
2026-03-04 01:18:02 +0530

    register route is added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
-import { HashRouter, Routes } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import AuthLayout from "./layouts/AuthLayout";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  
   return (
-     <HashRouter>
-      
+    <HashRouter>
+      <Routes>
         <Route element={<AuthLayout />}>
-         <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Route>
-      
+      </Routes>
     </HashRouter>
   );
 }
