b95f7ef317148f8db7ed5aa584f8285f26a878e3
Dushyant294 <yd.999017@gmail.com>
2026-03-04 17:56:30 +0530

    Added main layout and home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import AuthLayout from "./layouts/AuthLayout";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
         <Route element={<AuthLayout />}>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+        </Route>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
         </Route>
       </Routes>
     </HashRouter>
---
+++ client/src/layouts/MainLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import Topbar from "../components/Topbar";
+
+function MainLayout() {
+    return (
+        <div className="bg-white dark:bg-[#0b1220] text-black dark:text-white min-h-screen">
+            <Sidebar />
+
+            <div className="ml-64 pt-20">
+                <Topbar />
+                <main className="px-12 py-8 h-[calc(100vh-80px)] overflow-y-auto">
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    );
+}
+
+export default MainLayout;
