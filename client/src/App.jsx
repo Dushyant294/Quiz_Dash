a7d2f3a15d1e5cf393dffc138c9affaff07faed4
SOHAM-THUMMAR <sohamthummar04@gmail.com>
2026-03-10 00:00:29 +0530

    create quiz id added and route too and working on my quizes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import Leaderboard from "./pages/Leaderboard";
 import News from "./pages/News";
+import CreateQuiz from "./pages/CreateQuiz";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/explore" element={<Explore />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/news" element={<News />} />
+          <Route path="/create" element={<CreateQuiz />} />
         </Route>
       </Routes>
     </HashRouter>
