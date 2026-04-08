5d0a26986cc892bc3234fd356ad3d9de9a029770
Soham Thummar <sohamthummar04@gmail.com>
2026-04-08 12:59:17 +0530

    some fixes

--- Node_server/routes/userRoutes.js
+++ Node_server/routes/userRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// Define user routes
-router.get('/:id', userController.getUserProfile);
+// IMPORTANT: Specific routes MUST come before /:id to avoid being caught by the wildcard
 router.get('/dashboard/:id', userController.getDashboardData);
 router.get('/stats/:id', userController.getUserStats);
+router.get('/:id', userController.getUserProfile);
 
 module.exports = router;
